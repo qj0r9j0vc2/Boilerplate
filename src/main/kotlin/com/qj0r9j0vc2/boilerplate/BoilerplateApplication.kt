ef53dbb409c4c7578aea5b8c936ae3c7fdff1bce
jinu <jinwoo794533@gmail.com>
2022-03-19 00:14:52 +0900

    initial commit

---
+++ src/main/kotlin/com/qj0r9j0vc2/boilerplate/BoilerplateApplication.kt
@@ -0,0 +1,11 @@
+package com.qj0r9j0vc2.boilerplate
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BoilerplateApplication
+
+fun main(args: Array<String>) {
+    runApplication<BoilerplateApplication>(*args)
+}
