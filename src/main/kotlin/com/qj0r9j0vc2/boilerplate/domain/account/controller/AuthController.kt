101cb7ab21efd2d50d5f7442b72c64a6f0709be7
jinu <jinwoo794533@gmail.com>
2022-03-20 20:46:35 +0900

    :sparkles: controller

---
+++ src/main/kotlin/com/qj0r9j0vc2/boilerplate/domain/account/controller/AuthController.kt
@@ -0,0 +1,14 @@
+package com.qj0r9j0vc2.boilerplate.domain.account.controller
+
+import com.qj0r9j0vc2.boilerplate.domain.account.service.AuthService
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class AuthController(
+    private val authService: AuthService
+) {
+
+
+
+
+}
