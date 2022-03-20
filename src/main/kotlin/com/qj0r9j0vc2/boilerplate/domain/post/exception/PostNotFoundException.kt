a32d3d2959a01088aad84e848921ee350ff09720
jinu <jinwoo794533@gmail.com>
2022-03-21 02:15:43 +0900

    d

--- src/main/kotlin/com/qj0r9j0vc2/boilerplate/domain/post/exception/PostNotFoundException.kt
+++ src/main/kotlin/com/qj0r9j0vc2/boilerplate/domain/post/exception/PostNotFoundException.kt
@@ -3,5 +3,5 @@ package com.qj0r9j0vc2.boilerplate.domain.post.exception
 import com.qj0r9j0vc2.boilerplate.global.exception.base.GlobalException
 import com.qj0r9j0vc2.boilerplate.global.exception.base.data.type.ErrorCode
 
-class PostNotFoundException(data: String): GlobalException(ErrorCode.) {
+class PostNotFoundException(data: String): GlobalException(ErrorCode.POST_NOT_FOUND, data) {
 }
