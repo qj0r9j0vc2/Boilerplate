5a96d1b043da9440949fde16b54ae4354204e709
jinu <jinwoo794533@gmail.com>
2022-03-21 02:11:53 +0900

    getUser & Bean

--- src/main/kotlin/com/qj0r9j0vc2/boilerplate/global/util/CurrentToken.kt
+++ src/main/kotlin/com/qj0r9j0vc2/boilerplate/global/util/CurrentToken.kt
@@ -1,11 +1,25 @@
 package com.qj0r9j0vc2.boilerplate.global.util
 
+import com.qj0r9j0vc2.boilerplate.domain.account.data.entity.User
+import com.qj0r9j0vc2.boilerplate.domain.account.data.exception.UserNotFoundException
+import com.qj0r9j0vc2.boilerplate.domain.account.repository.UserRepository
+import com.qj0r9j0vc2.boilerplate.global.security.exception.InvalidJwtException
 import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.stereotype.Component
 
-class CurrentToken {
+
+@Component
+class CurrentToken(
+    private val userRepository: UserRepository
+
+) {
 
     fun getTokenCredentials(): String {
         return SecurityContextHolder.getContext().authentication.credentials.toString()
+    }
+
+    fun getUser(): User {
+        return userRepository.findByEmail(getTokenCredentials()).orElse(null)?: throw InvalidJwtException("null")
     }
 
 
